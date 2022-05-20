1e5677db9ce1f58d01f2390a7e6abe91fbae5163
j-lazo <j.org.ls@ciencias.unam.mx>
2022-05-20 18:14:28 +0200

    model utils functions

---
+++ scripts/models/model_utils.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import os
+from tensorflow.keras import applications
+import datetime
+
+class Checkpoint:
+    """Enhanced "tf.train.Checkpoint"."""
+
+    def __init__(self,
+                 checkpoint_kwargs,  # for "tf.train.Checkpoint"
+                 directory,  # for "tf.train.CheckpointManager"
+                 max_to_keep=5,
+                 keep_checkpoint_every_n_hours=None):
+        self.checkpoint = tf.train.Checkpoint(**checkpoint_kwargs)
+        self.manager = tf.train.CheckpointManager(self.checkpoint, directory, max_to_keep, keep_checkpoint_every_n_hours)
+
+    def restore(self, save_path=None):
+        save_path = self.manager.latest_checkpoint if save_path is None else save_path
+        return self.checkpoint.restore(save_path)
+
+    def save(self, file_prefix_or_checkpoint_number=None, session=None):
+        if isinstance(file_prefix_or_checkpoint_number, str):
+            return self.checkpoint.save(file_prefix_or_checkpoint_number, session=session)
+        else:
+            return self.manager.save(checkpoint_number=file_prefix_or_checkpoint_number)
+
+    def __getattr__(self, attr):
+        if hasattr(self.checkpoint, attr):
+            return getattr(self.checkpoint, attr)
+        elif hasattr(self.manager, attr):
+            return getattr(self.manager, attr)
+        else:
+            self.__getattribute__(attr)  # this will raise an exception
+
+
+def load_pretrained_backbones(name_model, weights='imagenet', include_top=False, trainable=False, new_name=None):
+
+    """
+    Loads a pretrained model given a name
+    :param name_model: (str) name of the model
+    :param weights: (str) weights names (default imagenet)
+    :return: sequential model with the selected weights
+    """
+
+    input_sizes_models = {'vgg16': (224, 224), 'vgg19': (224, 224), 'inception_v3': (299, 299),
+                          'resnet50': (224, 224), 'resnet101': (224, 244), 'mobilenet': (224, 224),
+                          'densenet121': (224, 224), 'xception': (299, 299)}
+
+    base_dir_weights = ''.join([os.getcwd(), '/scripts/models/weights_pretrained_backbones/'])
+    if name_model == 'vgg16':
+        weights_dir = base_dir_weights + 'vgg16/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.vgg16.VGG16(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    elif name_model == 'vgg19':
+        weights_dir = base_dir_weights + 'vgg19/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.vgg19.VGG19(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    elif name_model == 'inception_v3':
+        weights_dir = base_dir_weights + 'inception_v3/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.inception_v3.InceptionV3(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    elif name_model == 'resnet50':
+        weights_dir = base_dir_weights + 'resnet50/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.resnet50.ResNet50(include_top=include_top, weights=weights_dir)
+        base_model.trainable = True
+
+    elif name_model == 'resnet101':
+        weights_dir = base_dir_weights + 'resnet101/resnet101_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.resnet.ResNet101(include_top=include_top, weights=weights_dir)
+        base_model.trainable = True
+
+    elif name_model == 'mobilenet':
+        weights_dir = base_dir_weights + 'mobilenet/mobilenet_1_0_224_tf_no_top.h5'
+        base_model = applications.mobilenet.MobileNet(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    elif name_model == 'densenet121':
+        weights_dir = base_dir_weights + 'densenet/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.densenet.DenseNet121(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    elif name_model == 'xception':
+        weights_dir = base_dir_weights + 'xception/xception_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        base_model = applications.xception.Xception(include_top=include_top, weights=weights_dir)
+        base_model.trainable = trainable
+
+    else:
+        raise ValueError(f' MODEL: {name_model} not found')
+
+    new_base_model = tf.keras.models.clone_model(base_model)
+    new_base_model.set_weights(base_model.get_weights())
+
+    return new_base_model
+
+
