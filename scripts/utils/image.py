df8e504b5687001313b116931a40ec1835772037
j-lazo <j.org.ls@ciencias.unam.mx>
2022-05-24 14:52:37 +0200

    decode_image function fixed

--- scripts/utils/image.py
+++ scripts/utils/image.py
@@ -4,17 +4,9 @@ import tensorflow as tf
 def decode_image(file_name):
 
     image = tf.io.read_file(file_name)
-    if file_name.endswith('.png'):
-        image = tf.image.decode_png(image, channels=3)
-    elif file_name.endswith('.jpg'):
+    if tf.io.is_jpeg(image):
         image = tf.io.decode_jpeg(image, channels=3)
-    elif file_name.endswith('.jpeg'):
-        image = tf.io.decode_jpeg(image, channels=3)
-    elif file_name.endswith('.gif'):
-        image = tf.io.decode_gif(image)
-    elif file_name.endswith('.bmp'):
-        image = tf.io.decode_bmp(image, channels=3)
     else:
-        raise TypeError(f' Not able to decode {file_name}. File type {file_name[-4:]} not in image options')
+        image = tf.image.decode_png(image, channels=3)
 
     return image
