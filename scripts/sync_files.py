466e3517fc465a53be9646e356ed43be9452888a
j-lazo <j.org.ls@ciencias.unam.mx>
2022-05-31 12:02:20 +0200

    moved all the file management functions from sync_files to file_managemnet

--- scripts/sync_files.py
+++ scripts/sync_files.py
@@ -1,101 +1,13 @@
 import os
-
 import pandas as pd
 from absl import app, flags
 from absl.flags import FLAGS
 import shutil
-from paramiko import SSHClient
-from scp import SCPClient
 import tqdm
 import numpy as np
 import copy
-import yaml
-from matplotlib import pyplot as plt
-import seaborn as sns
-
-
-def analyze_individual_cases(name_csv_file):
-
-    df = pd.read_csv(name_csv_file)
-
-    fig1 = plt.figure(1, figsize=(11, 7))
-    ax1 = sns.boxplot(x="name_model", y="acc all", data=df)
-    ax1 = sns.swarmplot(x="name_model", y="acc all", data=df, color=".25")
-    ax1.set_ylim([0.5, 1.0])
-    ax1.title.set_text('')
-
-    plt.show()
-
-
-def read_yaml_file(path_file):
-    with open(path_file) as file:
-        document = yaml.full_load(file)
-
-    return document
-        #for item, doc in documents.items():
-        #    print(item, ":", doc)
-
-
-def move_files(name_file, destination_dir):
-
-    print(f'Moving file {name_file}')
-    current_path = os.path.join(os.getcwd(), name_file)
-    destination_path = os.path.join(destination_dir, name_file)
-    shutil.move(current_path, destination_path)
-    print(f'file_saved_at {destination_dir} ')
-
-
-def find_pattern_names(string_name, str_pattern):
-    """
-    Looks for a pattern name in a string and returns the number after it
-    :param string_name: the string where to look for a pattern
-    :param str_pattern: the pattern that needs to be found
-    :return:
-    """
-    match = re.search(str_pattern + '(\d+)', string_name)
-    if match:
-        return match.group(1)
-    else:
-        return np.nan
-
-
-def compress_files(dir_name, destination_dir=os.path.join(os.getcwd())):
-    print(f'Compressing: {dir_name} ... ')
-    output_name = os.path.split(dir_name)[-1]
-    shutil.make_archive(output_name, 'zip', dir_name)
-    move_files(output_name + '.zip', destination_dir)
-
-
-def read_txt_file(dir_file):
-    with open(dir_file, 'r') as f:
-        lines = f.readlines()
-    lines = [f.strip() for f in lines]
-    f.close()
-    return lines
-
-
-def check_file_exists(path_list, name_file='list_experiments.txt'):
-    list_files = os.listdir(path_list)
-    if name_file in list_files:
-        list_files = read_txt_file(os.path.join(path_list, name_file))
-        return list_files
-    else:
-        return None
-
-
-def transfer_files(local_host):
-
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
-    print(f'connecting to {local_host}')
-    ssh.connect(local_host)
-
-    # SCPCLient takes a paramiko transport as an argument
-    scp = SCPClient(ssh.get_transport())
-
-    #scp.put('test.txt', 'test2.txt')
-    #scp.get('test2.txt')
-    scp.close()
+from utils import data_analysis as daa
+from utils.data_management import file_management as dam
 
 
 def main(_argv):
@@ -111,6 +23,7 @@ def main(_argv):
     output_file_name = FLAGS.output_file_name
     gt_results_file = FLAGS.gt_results_file
     output_csv_file = FLAGS.output_csv_file
+    dir_to_analyze = FLAGS.dir_to_analyze
 
     if mode == 'prepare_files':
         compressed_results = os.path.join(os.getcwd(), 'results', 'bladder_tissue_classification_v2_compressed')
@@ -139,19 +52,18 @@ def main(_argv):
             # compress the whole file including weights
             if experiment_folder+'.zip' not in list_zipped_folders:
                 path_folder = os.path.join(folder_results, experiment_folder)
-                compress_files(path_folder, destination_dir=compressed_results)
+                dam.compress_files(path_folder, destination_dir=compressed_results)
                 # make a temporal folder with only the files you want to compress
                 temporal_folder_dir = os.path.join(os.getcwd(), 'results', 'remove_folders', experiment_folder)
                 os.mkdir(temporal_folder_dir)
                 # move files to temporal folder
                 list_files = [f for f in os.listdir(path_folder) if f.endswith('.png') or
                               f.endswith('.yaml') or f.endswith('.csv')]
+                # temporal folder to delete
                 for file_name in list_files:
                     shutil.copyfile(os.path.join(path_folder, file_name), os.path.join(temporal_folder_dir, file_name))
-                compress_files(temporal_folder_dir, destination_dir=transfer_results)
                 # compress the file
-                # delete the previous temporal one
-
+                dam.compress_files(temporal_folder_dir, destination_dir=transfer_results)
 
     elif mode == 'update_experiment_list':
 
@@ -164,7 +76,7 @@ def main(_argv):
                       if os.path.isdir(os.path.join(directory_path, f)) or f.endswith('.zip')]
         # remove repeated zip a and/or dir files
         unique_files = np.unique([f.replace('.zip', '') for f in list_files])
-        list_existing_files = check_file_exists(directory_path, name_file=output_file_name)
+        list_existing_files = dam.check_file_exists(directory_path, name_file=output_file_name)
         list_files_update = list(copy.copy(unique_files))
         for f in unique_files:
             if list_existing_files:
@@ -178,7 +90,7 @@ def main(_argv):
         f.close()
         print(f'list experiments file updated at dir: {directory_path}')
 
-    elif mode == 'unizp_and_analyze':
+    elif mode == 'unizp':
 
         # unzip the files that haven't been unzip
         dir_zipped_files = os.path.join(os.getcwd(), 'results', 'bladder_tissue_classification_v2_transfer')
@@ -193,13 +105,15 @@ def main(_argv):
             if zipped_file.replace('.zip', '') not in list_unzipped_files:
                 filename = os.path.join(dir_zipped_files, zipped_file)
                 dir_unzipped = os.path.join(dir_unzipped_files, zipped_file.replace('.zip', ''))
-                #if not os.path.isdir(dir_unzipped):
-                #    os.mkdir(dir_unzipped)
-                #print(f'Expanding {zipped_file}  ...')
-                #shutil.unpack_archive(filename, dir_unzipped)
+                if not os.path.isdir(dir_unzipped):
+                    os.mkdir(dir_unzipped)
+                print(f'Expanding {zipped_file}  ...')
+                shutil.unpack_archive(filename, dir_unzipped)
 
-        # analyze the yaml file
-
+    elif mode == 'analyze':
+        # analyze the yamls files
+        if not dir_to_analyze:
+            dir_unzipped_files = os.path.join(os.getcwd(), 'results', 'bladder_tissue_classification_v2')
         name_experiment = list()
         date_experiment = list()
         name_models = list()
@@ -221,8 +135,6 @@ def main(_argv):
             name_experiment.append(experiment_folder)
             date_experiment.append(date)
             name_models.append(name_model)
-            #print(learning_rate)
-            #learning_rates.append(learning_rate)
 
             if '+' in name_model:
                 backbone_name = name_model.split('+')[1]
@@ -235,7 +147,7 @@ def main(_argv):
             if yaml_file:
                 yaml_file = yaml_file.pop()
                 path_yaml = os.path.join(dir_unzipped_files, experiment_folder, yaml_file)
-                results = read_yaml_file(path_yaml)
+                results = dam.read_yaml_file(path_yaml)
                 acc_all = float(results['Accuracy ALL '])
                 acc_nbi = float(results['Accuracy NBI '])
                 acc_wli = float(results['Accuracy WLI '])
@@ -288,7 +200,7 @@ def main(_argv):
         sorted_df_wli = pd.DataFrame.from_dict(sorted_wli, orient='index')
         output_csv_file_dir4 = os.path.join(os.getcwd(), 'results', 'sorted_wli_experiments_information.csv')
         sorted_df_wli.to_csv(output_csv_file_dir4, index=False)
-        analyze_individual_cases(output_csv_file_dir)
+        daa.analyze_individual_cases(output_csv_file_dir)
 
     elif mode == 'transfer_results':
         list_files = os.listdir(local_path_results)
@@ -296,14 +208,14 @@ def main(_argv):
         # first compare which files are missing in local
         list_missing = list()
         if name_file_local in list_files:
-            list_files_local = read_txt_file(os.path.join(local_path_results, name_file_local))
-            list_files_remote = read_txt_file(os.path.join(local_path_results, name_file_remote))
+            list_files_local = dam.read_txt_file(os.path.join(local_path_results, name_file_local))
+            list_files_remote = dam.read_txt_file(os.path.join(local_path_results, name_file_remote))
             list_missing = [f for f in list_files_remote if f not in list_files_local]
             print(f'{len(list_missing)} need to be sync')
 
         for i, missing_file in enumerate(tqdm.tqdm(list_missing, desc='Preparing files')):
             print(f'Coping {missing_file} to local')
-            transfer_files(local)
+            dam.transfer_files(local)
 
 
 if __name__ == '__main__':
@@ -320,6 +232,7 @@ if __name__ == '__main__':
     flags.DEFINE_string('output_file_name', 'list_experiments.txt', 'name of the output file')
     flags.DEFINE_string('output_csv_file', 'experiments_information.csv', 'name of the output file')
     flags.DEFINE_string('gt_results_file', None, 'path to the gt values')
+    flags.DEFINE_string('dir_to_analyze', None, 'path to analyze all the experiements')
 
     try:
         app.run(main)
---
+++ scripts/utils/data_management/file_management.py
@@ -0,0 +1,76 @@
+import re
+import numpy as np
+import os
+from paramiko import SSHClient
+from scp import SCPClient
+import yaml
+import shutil
+
+
+def find_pattern_names(string_name, str_pattern):
+    """
+    Looks for a pattern name in a string and returns the number after it
+    :param string_name: the string where to look for a pattern
+    :param str_pattern: the pattern that needs to be found
+    :return:
+    """
+    match = re.search(str_pattern + '(\d+)', string_name)
+    if match:
+        return match.group(1)
+    else:
+        return np.nan
+
+
+def check_file_exists(path_list, name_file='list_experiments.txt'):
+    list_files = os.listdir(path_list)
+    if name_file in list_files:
+        list_files = read_txt_file(os.path.join(path_list, name_file))
+        return list_files
+    else:
+        return None
+
+
+def read_txt_file(dir_file):
+    with open(dir_file, 'r') as f:
+        lines = f.readlines()
+    lines = [f.strip() for f in lines]
+    f.close()
+    return lines
+
+
+def compress_files(dir_name, destination_dir=os.path.join(os.getcwd())):
+    print(f'Compressing: {dir_name} ... ')
+    output_name = os.path.split(dir_name)[-1]
+    shutil.make_archive(output_name, 'zip', dir_name)
+    move_files(output_name + '.zip', destination_dir)
+
+
+def transfer_files(local_host):
+
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    print(f'connecting to {local_host}')
+    ssh.connect(local_host)
+
+    # SCPCLient takes a paramiko transport as an argument
+    scp = SCPClient(ssh.get_transport())
+
+    #scp.put('test.txt', 'test2.txt')
+    #scp.get('test2.txt')
+    scp.close()
+
+
+def read_yaml_file(path_file):
+    with open(path_file) as file:
+        document = yaml.full_load(file)
+
+    return document
+
+
+def move_files(name_file, destination_dir):
+
+    print(f'Moving file {name_file}')
+    current_path = os.path.join(os.getcwd(), name_file)
+    destination_path = os.path.join(destination_dir, name_file)
+    shutil.move(current_path, destination_path)
+    print(f'file_saved_at {destination_dir} ')
